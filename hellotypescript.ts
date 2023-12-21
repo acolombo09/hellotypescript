63a6e4ede5951f1f573cfa7a0f68bb7204142481
acolombo09 <acolombo0911@gmail.com>
2023-12-21 11:06:45 +0100

    variables_declarations

---
+++ hellotypescript.ts
@@ -0,0 +1,17 @@
+// Typescript single line comment
+
+/* 
+Typescript multiline comment
+*/
+
+var message : string = "Hello World";
+console.log(message);
+
+// Different ways of variable declaration
+var a : number ;    // only type declaration
+var b = 10;     // only initial value
+var c : number = 10;    // both type and value
+var d;      // without the type and the initial value
+
+// how to assign value to a variable
+var x : number = 100;
