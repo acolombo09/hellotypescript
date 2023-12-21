856069543689801a486fb379d258a6db03a67e33
acolombo09 <acolombo0911@gmail.com>
2023-12-21 11:44:46 +0100

    variables_declarations

--- hellotypescript.ts
+++ hellotypescript.ts
@@ -15,3 +15,69 @@ var d;      // without the type and the initial value
 
 // how to assign value to a variable
 var x : number = 100;
+
+// 'var'
+
+// #1 var can be declared without being initialised
+var name1 : string;     // no error
+
+// #2 var variables can be re-declared and updated
+var name11 : string = "Hello";
+console.log("var Initialised :", name11);
+name11 = "Hello Typescript";
+console.log("var updated", name11);
+var name11 : string = "Welcome to Typescript";
+console.log("var re-declared", name11);
+
+// #3 var variables are function/global scoped
+function example1() {
+    if(true){
+    var x = 100;
+    console.log(x);
+  }
+  console.log(x); // i can print with no error
+}
+
+// 'let'
+
+// #1  let can be declared without being initialised
+let name2 : string;     // no error
+
+// #2 let variables can be updated but cannot be re-declared
+let name22 : string = "Hello";
+console.log("let initialised :", name22);
+name22 = "hello typescript";
+console.log("let updated", name22);
+// let name22   error
+
+// #3 let variables are block scoped
+function example2() {
+    if(true){
+    let x1 = 100;
+    console.log(x1); // block scope no error
+  }
+  // console.log(x1); // error
+}
+
+// 'const'
+
+// #1 const must be initialised during declaration
+// const name3 : string;    error
+const name3 : string = "Hello";     //no error
+
+// #2 const cannot be updated or re-declared
+const name33 : string = "hello";
+console.log("const initialised :", name33);
+// name33 = "hello typescript";     error
+// console.log("const updated", name33);
+// var name33 : string = "welcome to typescript";   error
+// console.log("const re-declared", name33);
+
+// #3 const variables are block scoped
+function example3() {
+  if(true){
+    const x3 = 100;
+    console.log(x3); // block scope no error
+  }
+  // console.log(x3); // error
+}
